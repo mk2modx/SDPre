40e02f96ab7f482267636226d8233b6108f84a8f
Mark Ag <mk2modx@icloud.com>
2019-06-22 16:31:41 -0600

    jthw ex 4 Study Drill

---
+++ JavaTheHardWay/PrintingChoicesEscapes.java
@@ -0,0 +1,12 @@
+public class PrintingChoicesEscapes {
+	public static void main( String[] args ) {
+		//This exercise demonstrates escape sequences & comments (like these)!
+		System.out.print( "Learn\tJava\n\tthe\nHard\tWay\n\n" + "\tLearn Java the \"Hard\" Way!\n"
+		+ "Hello\n" + "Jello\by\n" + "Hard to believe, eh?\n" + "Surprised? /* abcde */ Or what did you expect?\n"
+		+ "\\// -=- \\//\n" + "\\\\ \\\\\\ \\\\\\\\\n" + "I hope you understand \"escape sequences\" now. \n");
+
+
+	}
+}
+
+	
