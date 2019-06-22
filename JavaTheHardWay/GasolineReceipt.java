53d6d6c937b32a285330dd46c18e27fee60679c8
Mark Ag <mk2modx@icloud.com>
2019-06-22 15:52:53 -0600

    Finish jthw ex2

---
+++ JavaTheHardWay/GasolineReceipt.java
@@ -0,0 +1,16 @@
+public class GasolineReceipt {
+	public static void main ( String[] args ) {
+		System.out.println( "+-----------------------------+" );
+		System.out.println( "|                             |" );
+		System.out.println( "|      MARK's CONOCO          |" );
+		System.out.println( "|                             |" );
+		System.out.println( "|  2019-06-19   09:33PM       |" );
+		System.out.println( "|                             |" );
+		System.out.println( "|  Gallons:         10.870    |" );
+		System.out.println( "|  Price/gallon:   $ 2.80     |" );
+		System.out.println( "|                             |" );
+		System.out.println( "|  Fuel total:    $ 30.44     |" );
+		System.out.println( "|                             |" );
+		System.out.println( "+-----------------------------+" );
+		}
+	}
