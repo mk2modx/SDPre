3d3dff0b97e0c751e01240b20eab36dcbdf2b0e1
Mark Ag <mk2modx@icloud.com>
2019-06-22 16:45:01 -0600

    O1L3

---
+++ Labs/Objective1Lab3.java
@@ -0,0 +1,9 @@
+public class Objective1Lab3 {
+  public static void main ( String [] args ){
+
+  System.out.println( "Name : Mark Agbayani");
+  System.out.println( "Age : 37");
+  System.out.println( "Colorado");
+
+  }
+}
