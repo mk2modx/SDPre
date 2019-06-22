49c33eeadab352ce25f7bdb51de4aa7ff7504a91
Mark Ag <mk2modx@icloud.com>
2019-06-22 06:15:11 -0600

    Finish jthw ex 1

---
+++ JavaTheHardWay/FirstProg.java
@@ -0,0 +1,6 @@
+public class FirstProg {
+  public static void main( String[] args ) {
+      System.out.println( "I am determined to leaRN HOW TO CODE." );
+      System.out.println( "Today's date is June 22, 2019" );
+  }
+}
