7429042d28bc8c55a8854bf71b11d6fddf1683f0
Mark Ag <mk2modx@icloud.com>
2019-06-23 19:12:13 -0600

    O2L2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,13 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Mark Agbayani";
+    int age = 37;
+    String state = "Colorado";
+
+    System.out.println("My name is " + name + ".");
+    System.out.println("I am " + age + " years old.");
+    System.out.println("Currently living in " + state + ".");
+
+
+      }
+}
