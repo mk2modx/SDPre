a180dfe4b5179f3a80039d483645afed43f49e41
Mark Ag <mk2modx@icloud.com>
2019-06-23 19:20:25 -0600

    O2L3

---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,13 @@
+public class Objective2Lab3 {
+  public static void main(String[] args) {
+    String food1, food2, food3;
+    food1 = "pizzas";
+    food2 = "burgers";
+    food3 = "chocolate";
+
+    System.out.println("My Favorite Foods are:");
+    System.out.println("1. " + food1);
+    System.out.println("2. " + food2);
+    System.out.println("3. " + food3);
+  }
+}
