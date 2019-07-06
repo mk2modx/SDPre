bcc39347e50f15e3b713aa4fa4d28a423929c5bd
Mark Ag <mk2modx@icloud.com>
2019-07-06 15:10:04 -0600

    O6L4

---
+++ Labs/Objective6Lab4.java
@@ -0,0 +1,17 @@
+public class Objective6Lab4 {
+  public static void main(String[] args) {
+
+    int counter, sum;
+
+    counter = 1;
+    sum = 0;
+
+    while(counter <=20) {
+
+
+      counter = counter + 1;
+      sum += counter;
+    }
+    System.out.println(counter + sum);
+  }
+}
